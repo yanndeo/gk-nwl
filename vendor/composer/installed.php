6773a135fe95d0898f78766ba2033fd5e4fce904
Yann Deo <yan2sambou@gmail.com>
2021-06-19 18:04:15 +0100

    fix(autoload): remove vendor folder to gitignore file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '437dad7c670a49703ee09f7ff0e129db7891d597',
+    'name' => 'maranatha/newsletter-plugin',
+  ),
+  'versions' => 
+  array (
+    'maranatha/newsletter-plugin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '437dad7c670a49703ee09f7ff0e129db7891d597',
+    ),
+  ),
+);
